9d6867576d1cd87bae8e1366dd89c8bb7f4d793b
Ram Sharma <ram_sharma@medilink.ph>
2023-07-09 10:12:39 +0530

    add link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,8 @@ export default function Home() {
         
       </div> */}
       
-        {/* <a
+        <div>
+          <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -109,7 +110,8 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
-        </a> */}
+        </a>
+        </div>
         <h1>Learn Next.js!</h1>
         <p>Get started by editing page.tsx</p>
       
